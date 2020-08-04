cad6f16835054fd2641f9e4f4382a9f87d66d295
Keita Endo <endo@akagi777.com>
2020-08-04 16:35:50 +0000

    0805

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -1,20 +1,41 @@
 <?php
-
 namespace App\Http\Controllers\Admin;
-
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-
+use App\News;
 class NewsController extends Controller
 {
-    //
-    public function add()
-    {
-    return view('admin.news.create');
-    }
-    public function create(Request $request)
+  public function add()
   {
-      // admin/news/createにリダイレクトする
+      return view('admin.news.create');
+  }
+  public function create(Request $request)
+  {
+    $this->validate($request, News::$rules);
+    $news = new News;
+    $form = $request->all();
+      if (isset($form['image'])) {
+        $path = $request->file('image')->store('public/image');
+        $news->image_path = basename($path);
+      } else {
+          $news->image_path = null;
+      }
+      unset($form['_token']);
+      unset($form['image']);
+      $news->fill($form);
+      $news->save();
       return redirect('admin/news/create');
-  }  
+  }
+  public function index(Request $request)
+  {
+      $cond_title = $request->cond_title;
+      if ($cond_title != '') {
+          // 検索されたら検索結果を取得する
+          $posts = News::where('title', $cond_title)->get();
+      } else {
+          // それ以外はすべてのニュースを取得する
+          $posts = News::all();
+      }
+      return view('admin.news.index', ['posts' => $posts, 'cond_title' => $cond_title]);
+  }
 }
