82743f5a0103958cb33de69d691927a65c8b5f9d
Keita Endo <endo@akagi777.com>
2020-08-06 01:37:41 +0000

    0806

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -15,21 +15,39 @@ class ProfileController extends Controller
     }
     public function create(Request $request)
     {
+        // Varidationを行う
         $this->validate($request, profile::$rules);
         $profile = new profile;
         $form = $request->all();
+        
+        
         unset($form['_token']);
-        unset($form['image']);
+        // データベースに保存する
         $profile->fill($form);
         $profile->save();
         return redirect('admin/profile/create');
     }
-    public function edit()
+    public function edit(Request $request)
     {
-        return view('admin.profile.edit');
+        $profile = Profile::find($request->id);
+      if (empty($profile)) {
+        abort(404);    
+      }
+        return view('admin.profile.edit', ['profile_form' => $profile]);
     }
-    public function update()
+    
+    public function update(Request $request)
     {
+        // Validationをかける
+      $this->validate($request, Profile::$rules);
+      // News Modelからデータを取得する
+      $profile = Profile::find($request->id);
+      // 送信されてきたフォームデータを格納する
+      $profile_form = $request->all();
+      unset($profile_form['_token']);
+
+      // 該当するデータを上書きして保存する
+      $profile->fill($profile_form)->save();
         return redirect('admin/profile/edit');
     }
     
