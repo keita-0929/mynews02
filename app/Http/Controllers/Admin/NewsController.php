c0654263871b3013deb96ba594e8163f534a2264
Keita Endo <endo@akagi777.com>
2020-08-16 07:47:52 +0000

    0816?修正2

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -32,8 +32,8 @@ class NewsController extends Controller
       // formに画像があれば、保存する
       \Debugbar::info($form);
       if (isset($form['image'])) {
-        $path = $request->file('image')->store('public/image');
-        $news->image_path = basename($path);
+        $path = Storage::disk('s3')->putFile('/',$form['image'],'public');
+        $news->image_path = Storage::disk('s3')->url($path);
       } else {
         $news->image_path = null;
       }
